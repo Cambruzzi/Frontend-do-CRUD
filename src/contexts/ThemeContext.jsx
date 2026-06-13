ab0cc04810d6128f272c7df4f0b4d34aab962d76
Cambruzzi <leocfritz@gmail.com>
2026-06-13 09:54:07 -0300

    feat: adiciona responsividade para celulares e tablets nas telas de autenticacao e produtos

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,5 +1,7 @@
 import { createContext, useState, useContext, useEffect } from 'react';
+
 const ThemeContext = createContext();
+
 /**
  * Provedor do Tema. Ele deve envelopar a aplicação para distribuir
  * o estado de Dark/Light mode para todas as telas.
@@ -10,12 +12,21 @@ export function ThemeProvider({ children }) {
   });
   useEffect(() => {
     localStorage.setItem('temaClaro', temaClaro);
+    const elementoRoot = document.getElementById('root');
+    if (temaClaro) {
+      elementoRoot.classList.add('tema-claro');
+    } else {
+      elementoRoot.classList.remove('tema-claro');
+    }
   }, [temaClaro]);
+
   const alternarTema = () => setTemaClaro(!temaClaro);
+
   return (
     <ThemeContext.Provider value={{ temaClaro, alternarTema }}>
       {children}
     </ThemeContext.Provider>
   );
 }
+
 export const useTheme = () => useContext(ThemeContext);
